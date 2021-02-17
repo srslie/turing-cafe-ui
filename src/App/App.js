7da793622e29a68b8bf89c8039424e7291817e46
srslie <aliceruppert@gmail.com>
2021-02-17 09:39:28 -0700

    Add conditional rendering for loading, error, res

--- src/App/App.js
+++ src/App/App.js
@@ -11,13 +11,26 @@ class App extends Component {
       error: ''
     }
   }
-  
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <Form />
-        <Reservations />
+        {!this.state.reservations && !this.state.error &&
+          <p>Loading...</p>
+        }
+        {this.state.error &&
+          <p>{this.state.error}</p>
+        }
+        
+        {this.state.reservations &&
+        <>
+          <Form />
+          <Reservations reservations={this.state.reservations}/>
+        </>
+        }
+
+      
       </div>
     )
   }
