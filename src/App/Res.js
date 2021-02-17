dfec8544c791fc9c5132f86d5000bc3eaa0aa734
srslie <aliceruppert@gmail.com>
2021-02-17 10:45:24 -0700

    Add id and fix cancel

--- src/App/Res.js
+++ src/App/Res.js
@@ -5,17 +5,17 @@ const Res = ({res, cancelRes}) => {
 
   const cancel = (event) => {
     event.preventDefault()
-    console.log(event.currentTarget)
-    cancelRes(event.currentTarget)
+    const targetId = event.target.closest('.res').id
+    cancelRes(targetId)
   }
 
   return (
-    <div className="res" id={Date.now} >
+    <div className="res" id={res.id} key={res.id} >
       <h2 className="name">{res.name}</h2>
       <p className="date">{res.date}</p>
       <p className="time">{res.time}</p>
       <p className="num">{res.num}</p>
-      <button type="button" onClick={this.cancelRes}>Cancel</button>
+      <button type="button" onClick={cancel}>Cancel</button>
     </div>
   )
 }
