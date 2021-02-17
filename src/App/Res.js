f47a6294fd310118a508749d1d9b3fe2925ada0a
srslie <aliceruppert@gmail.com>
2021-02-17 10:06:12 -0700

    Add structure of res card, button

--- src/App/Res.js
+++ src/App/Res.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Res.css';
 
-const Res = () = {
+const Res = ({res, cancelRes}) => {
 
-  return(
-    
+  const cancel = (event) => {
+    event.preventDefault()
+    console.log(event.currentTarget)
+    cancelRes(event.currentTarget)
+  }
+
+  return (
+    <div className="res" id={Date.now} >
+      <h2 className="name">{res.name}</h2>
+      <p className="date">{res.date}</p>
+      <p className="time">{res.time}</p>
+      <p className="num">{res.num}</p>
+      <button type="button" onClick={this.cancelRes}>Cancel</button>
+    </div>
   )
 }
 
