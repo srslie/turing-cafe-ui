81602ca957ac0a6a2466bbaa3c70985455826e20
srslie <aliceruppert@gmail.com>
2021-02-17 10:06:39 -0700

    Add map and pass props

--- src/App/Reservations.js
+++ src/App/Reservations.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Reservations.css';
 import Res from './Res'
 
-const Reservations = () = {
-
-  return(
-
+const Reservations = ({reservations, cancelRes}) => {
+  return (
+    <div className="reservations">
+      {reservations.map(reservation => <Res res={reservation} cancelRes={cancelRes}/>)}
+    </div>
   )
 }
 
