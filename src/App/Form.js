46a24cfc84b512a2e9c261480420013b103c6634
srslie <aliceruppert@gmail.com>
2021-02-17 10:00:52 -0700

    Add form state, inputs, handlesubmit and handlechange

--- src/App/Form.js
+++ src/App/Form.js
@@ -5,18 +5,42 @@ class Form extends Component {
   constructor(){
     super();
     this.state = {
-
+      nameInput: null,
+      dateInput: null,
+      timeInput: null,
+      numInput: null
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   } 
 
-  handleClick() {
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
 
+  handleSubmit(event) {
+    alert('Thank you for your reservation!')
+    event.preventDefault()
   }
 
   render() {
     return (
+      <form onSubmit={this.handleSubmit}>
+        <label for="name-input"></label>
+        <input className="name-input" type="text" placeholder="Name" value={this.state.nameInput} onChange={this.handleChange} required></input>
 
+        <label for="date-input"></label>
+        <input className="date-input" type="date" value={this.state.dateInput} onChange={this.handleChange} required></input>
 
+        <label for="time-input"></label>
+        <input className="time-input" type="time" value={this.state.timeInput}  onChange={this.handleChange} required></input>
+
+        <label for="num-input"></label>
+        <input className="num-input" type="number" placeholder="2" min="1" max="20" value={this.state.numInput} onChange={this.handleChange} required></input>
+
+        <button type="submit">Make Reservation</button>
+      </form>
     )
   }
 }
